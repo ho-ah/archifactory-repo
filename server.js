b0fb5e8b1e262156ea3d3fd8f01faff6b6cf4f77
ho-ya <yousos04@gmail.com>
2022-02-16 10:04:31 +0900

    불필요한 폴더 및 파일 삭제, index.html 수정

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: true})) 
 
-app.listen(8000, function(){
-    console.log('listening on 8000');
+app.listen(443, function(){
+    console.log('listening on 443');
 });
 
 app.get('/', function(요청, 응답){
-    응답.sendFile(__dirname + '/public/index.html')
+    응답.sendFile(__dirname + 'index.html')
 });
 
+app.get('/project', function(요청, 응답){
+    응답.sendFile(__dirname + 'project.html')
+})
 
-
-
+app.post('/contactadd', function(){
+    응답.send('전송완료');
+    console.log(요청.body);
+    
+});
 
