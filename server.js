9343b1e6e98c74c898ea163a2f34738fb6460a27
ho-ya <yousos04@gmail.com>
2022-02-28 16:24:10 +0900

    ejs 설치, 어퍼하우스 상단으로 수정

--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 
+const MongoClient = require('mongodb').MongoClient;
 
+var db;
+MongoClient.connect('mongodb+srv://Teamfactory:tf04060925@cluster0.ioxfy.mongodb.net/Archifactory?retryWrites=true&w=majority', {useUnifiedTopology: true}, function(에러, clinet){
+    if (에러) return console.log(에러)
+    db = clinet.db('Archifactory');
 
-app.listen(443, function(){
-    console.log('listening on 443');
+    db.collection('project').insertOne({이름 : 'Hong', 나이 : 28}, function(에러, 결과){
+        console.log('저장완료');
+    });
+
+    app.listen(443, function(){
+        console.log('listening on 443');
+    });
 });
+
 
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + 'index.html')
@@ -22,6 +34,6 @@ app.get('/project', function(요청, 응답){
 
 app.post('/contactadd', function(요청, 응답){
     응답.send('전송완료');
-    console.log(요청.body);
+    console.log(요청.body); 
 });
 
