e787bf1981988fd84339e1ec180fc9cefa25e16e
ho-ya <yousos04@gmail.com>
2022-02-28 09:38:50 +0900

    오류 대량 수정

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({extended: true}));
+
+
 
 app.listen(443, function(){
     console.log('listening on 443');
@@ -17,9 +20,8 @@ app.get('/project', function(요청, 응답){
     응답.sendFile(__dirname + 'project.html')
 })
 
-app.post('/contactadd', function(){
+app.post('/contactadd', function(요청, 응답){
     응답.send('전송완료');
     console.log(요청.body);
-    
 });
 
